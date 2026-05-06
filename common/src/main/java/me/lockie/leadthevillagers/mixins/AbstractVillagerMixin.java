b0e6e22233d2ffa1dc72c179257b24956b588052
Sherlock Luk <sherlockluk@outlook.com>
2026-05-06 12:12:38 +0100

    Porting to 1.21.11

--- common/src/main/java/me/lockie/leadthevillagers/mixins/AbstractVillagerMixin.java
+++ common/src/main/java/me/lockie/leadthevillagers/mixins/AbstractVillagerMixin.java
@@ -1,6 +1,6 @@
 package me.lockie.leadthevillagers.mixins;
 
-import net.minecraft.world.entity.npc.AbstractVillager;
+import net.minecraft.world.entity.npc.villager.AbstractVillager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
